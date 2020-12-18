c18da533b0a43277f58d42e89d2cf23e3bced59c
Michelangelo <mhlmrt@gmail.com>
2020-12-18 12:24:21 +0100

    Implemented class Office, Order, Service

--- src/main/java/Office.java
+++ src/main/java/Office.java
@@ -1,4 +1,61 @@
 package main.java;
 
 public class Office {
+    String city;
+    String region;
+    String id;
+    String name;
+    Integer capacity;
+
+    public Office(String city, String region, String name, String id, Integer capacity){
+        this.city=city;
+        this.region=region;
+        this.name = name;
+        this.id=id;
+        this.capacity=capacity;
+    }
+
+    public Office(){};
+
+    public String getCity() {
+        return city;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+
 }
--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,4 +1,104 @@
 package main.java;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Order {
+    Car car = new Car();
+    User user = new User();
+    Date pickDate = new Date();
+    Date deliveryDate = new Date();
+    Office pickOffice = new Office();
+    Office deliveryOffice = new Office();
+    List<Service> accessories = new ArrayList<Service>();
+    Double priceCar;
+    Double priceAccessories;
+    
+    public Order(Car car, User user, Double priceCar, Date pickDate, Office pickOffice,Date deliveryDate, Office deliveryOffice, Double priceAccessories, List<Service> accessories) {
+        this.car=car;
+        this.user=user;
+        this.priceCar=priceCar;
+        this.pickDate=pickDate;
+        this.pickOffice=pickOffice;
+        this.deliveryDate=deliveryDate;
+        this.deliveryOffice=deliveryOffice;
+        this.priceAccessories=priceAccessories;
+        this.accessories=accessories;
+    }
+
+    public Order(){}
+    
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public List<Service> getAccessories() {
+        return accessories;
+    }
+
+    public Date getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public Date getPickDate() {
+        return pickDate;
+    }
+
+    public Double getPriceCar() {
+        return priceCar;
+    }
+
+    public Double getPriceAccessories() {
+        return priceAccessories;
+    }
+
+    public Office getDeliveryOffice() {
+        return deliveryOffice;
+    }
+
+    public Office getpickOffice() {
+        return pickOffice;
+    }
+
+    public void setDeliveryOffice(Office deliveryOffice) {
+        this.deliveryOffice = deliveryOffice;
+    }
+
+    public void setAccessories(List<Service> accessories) {
+        this.accessories = accessories;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setDeliveryDate(Date deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    public void setPickDate(Date pickDate) {
+        this.pickDate = pickDate;
+    }
+
+    public void setPriceAccessories(Double priceAccessories) {
+        this.priceAccessories = priceAccessories;
+    }
+
+    public void setPriceCar(Double priceCar) {
+        this.priceCar = priceCar;
+    }
+
+    public void setpickOffice(Office pickOffice) {
+        this.pickOffice = pickOffice;
+    }
+
 }
